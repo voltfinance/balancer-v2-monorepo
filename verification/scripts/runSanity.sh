2edaf281a447723cc4698204b027579bb18b1cd2
urikirsh <uri@certora.com>
2021-04-11 11:05:55 +0300

    sanity basic run works

--- verification/scripts/runSanity.sh
+++ verification/scripts/runSanity.sh
@@ -1,6 +1,7 @@
 certoraRun contracts/vault/Vault.sol \
   --verify Vault:verification/spec/sanity.spec \
   --solc solc7.6 \
-  --settings -t=300 \
+  --settings -t=300,-recursionEntryLimit=6 \
+  --cache balancer_sanity \
   --debug \
   --staging --msg "Vault sanity"
