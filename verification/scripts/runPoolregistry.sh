92a4d6f59e46d837c9133fde2a197fca9cad6f66
Shelly Grossman <shellygr151@gmail.com>
2021-04-11 16:39:09 +0300

    sanity checks

--- verification/scripts/runPoolregistry.sh
+++ verification/scripts/runPoolregistry.sh
@@ -2,5 +2,5 @@ certoraRun verification/harness/PoolRegistryHarness.sol \
     --verify PoolRegistryHarness:verification/spec/poolregistry.spec \
     --solc solc7.6 \
     --cache balancerPoolRegistryBv \
-    --settings -useBitVectorTheory \
+    --settings -useBitVectorTheory,-ruleSanityChecks \
     --msg "Pool registry, bitvector analysis, pool id/address conversions invertible"
