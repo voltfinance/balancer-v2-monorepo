de093648952d2bc04a7ff2b8117439c60be73897
urikirsh <uri@certora.com>
2021-04-19 11:54:34 +0300

    fixed harness

--- verification/scripts/applyHarness.sh
+++ verification/scripts/applyHarness.sh
@@ -5,6 +5,7 @@ perl -0777 -i -pe 's/private/internal/g' contracts/vault/PoolRegistry.sol
 # External to public
 perl -0777 -i -pe 's/external/public/g' contracts/vault/UserBalance.sol
 perl -0777 -i -pe 's/external/public/g' contracts/vault/FlashLoans.sol
+perl -0777 -i -pe 's/external/public/g' contracts/vault/PoolBalances.sol
 
 # Virtualize
 perl -0777 -i -pe 's/_calculateFlashLoanFee\(uint256 amount\) internal/_calculateFlashLoanFee\(uint256 amount\) virtual internal/g' contracts/vault/Fees.sol
@@ -14,34 +15,37 @@ perl -0777 -i -pe 's/function \_validateSignature\(address user, uint256 errorCo
 perl -0777 -i -pe 's/\_calculateFlashLoanFeeAmount\(uint256 amount\) internal view/\_calculateFlashLoanFeeAmount\(uint256 amount\) internal virtual view/g' contracts/vault/Fees.sol
 
 # sendValue => nothing inteface
-perl -0007 -i -pe 's/recipient\.sendValue\(amount\)/Nothing\(recipient\)\.nop{value:amount}\(\)/g' contracts/vault/AssetTransfersHandler.sol
-perl -0007 -i -pe 's/msg\.sender\.sendValue\(excess\)/Nothing\(msg\.sender\)\.nop{value:excess}\(\)/g' contracts/vault/AssetTransfersHandler.sol
-perl -0007 -i -pe 's/;\s*abstract contract/;
+perl -0777 -i -pe 's/recipient\.sendValue\(amount\)/Nothing\(recipient\)\.nop{value:amount}\(\)/g' contracts/vault/AssetTransfersHandler.sol
+perl -0777 -i -pe 's/msg\.sender\.sendValue\(excess\)/Nothing\(msg\.sender\)\.nop{value:excess}\(\)/g' contracts/vault/AssetTransfersHandler.sol
+perl -0777 -i -pe 's/;\s*abstract contract/;
 interface Nothing { function nop\(\) external payable; }
 abstract contract/g' contracts/vault/AssetTransfersHandler.sol
 
 # remove problematic modifiers
-perl -0007 -i -pe 's/nonReentrant //g' contracts/vault/Swaps.sol
-perl -0007 -i -pe 's/noEmergencyPeriod //g' contracts/vault/Swaps.sol
-perl -0007 -i -pe 's/nonReentrant //g' contracts/vault/PoolBalances.sol
-perl -0007 -i -pe 's/noEmergencyPeriod //g' contracts/vault/PoolBalances.sol
+perl -0777 -i -pe 's/nonReentrant //g' contracts/vault/Swaps.sol
+perl -0777 -i -pe 's/noEmergencyPeriod //g' contracts/vault/Swaps.sol
+perl -0777 -i -pe 's/nonReentrant //g' contracts/vault/PoolBalances.sol
+perl -0777 -i -pe 's/noEmergencyPeriod //g' contracts/vault/PoolBalances.sol
 
 # simplifying BallanceAllocation
-perl -007 -i -pe 's/uint256 mask = 2\*\*\(224\) - 1;//g' contracts/vault/balances/BalanceAllocation.sol
-perl -007 -i -pe 's/uint256 mask = 2\*\*\(32\) - 1;//g' contracts/vault/balances/BalanceAllocation.sol
-perl -007 -i -pe 's/uint256 mask = 2\*\*\(112\) - 1;//g' contracts/vault/balances/BalanceAllocation.sol
-perl -007 -i -pe 's/uint256 mask = 2\*\*\(112\) - 1;//g' contracts/vault/balances/BalanceAllocation.sol
+perl -0777 -i -pe 's/uint256 mask = 2\*\*\(224\) - 1;//g' contracts/vault/balances/BalanceAllocation.sol
+perl -0777 -i -pe 's/uint256 mask = 2\*\*\(32\) - 1;//g' contracts/vault/balances/BalanceAllocation.sol
+perl -0777 -i -pe 's/uint256 mask = 2\*\*\(112\) - 1;//g' contracts/vault/balances/BalanceAllocation.sol
+perl -0777 -i -pe 's/uint256 mask = 2\*\*\(112\) - 1;//g' contracts/vault/balances/BalanceAllocation.sol
 
-perl -007 -i -pe 's/return \(uint256\(balance\) & mask\) == 0/return uint256\(balance\) == 0/g' contracts/vault/balances/BalanceAllocation.sol
-perl -007 -i -pe 's/return uint256\(balance >> 224\) & mask/return 0/g' contracts/vault/balances/BalanceAllocation.sol
-perl -007 -i -pe 's/return uint256\(balance >> 112\) & mask/return 0/g' contracts/vault/balances/BalanceAllocation.sol
-perl -007 -i -pe 's/return uint256\(balance\) & mask/return uint256\(balance\)/g' contracts/vault/balances/BalanceAllocation.sol
-perl -007 -i -pe 's/\(\(\_mostSignificant << 224\) \+ \(\_midSignificant << 112\) \+ \_leastSignificant\)/\(\_leastSignificant\)/g' contracts/vault/balances/BalanceAllocation.sol
-perl -007 -i -pe 's/& mask//g' contracts/vault/balances/BalanceAllocation.sol
+perl -0777 -i -pe 's/return \(uint256\(balance\) & mask\) == 0/return uint256\(balance\) == 0/g' contracts/vault/balances/BalanceAllocation.sol
+perl -0777 -i -pe 's/return uint256\(balance >> 224\) & mask/return 0/g' contracts/vault/balances/BalanceAllocation.sol
+perl -0777 -i -pe 's/return uint256\(balance >> 112\) & mask/return 0/g' contracts/vault/balances/BalanceAllocation.sol
+perl -0777 -i -pe 's/return uint256\(balance\) & mask/return uint256\(balance\)/g' contracts/vault/balances/BalanceAllocation.sol
+perl -0777 -i -pe 's/\(\(\_mostSignificant << 224\) \+ \(\_midSignificant << 112\) \+ \_leastSignificant\)/\(\_leastSignificant\)/g' contracts/vault/balances/BalanceAllocation.sol
+perl -0777 -i -pe 's/& mask//g' contracts/vault/balances/BalanceAllocation.sol
 
 # remove swap deadline
-perl -007 -i -pe 's/       \_require\(block.timestamp <= deadline, Errors.SWAP_DEADLINE\)/     \/\/   \_require\(block.timestamp <= deadline, Errors.SWAP_DEADLINE\)/g' contracts/vault/Swaps.sol
-# joinPool and exitPool to public
-perl -0777 -i -pe 's/external/public/g' contracts/vault/PoolBalances.sol
+perl -0777 -i -pe 's/       \_require\(block.timestamp <= deadline, Errors.SWAP_DEADLINE\)/     \/\/   \_require\(block.timestamp <= deadline, Errors.SWAP_DEADLINE\)/g' contracts/vault/Swaps.sol
+
 # _validateTokensAndGetBalances to internal
-perl -0777 -i -pe 's/function \_validateTokensAndGetBalances\(bytes32
+perl -0777 -i -pe 's/_validateTokensAndGetBalances\(bytes32 poolId, IERC20\[\] memory expectedTokens\)
+        private
+        view/_validateTokensAndGetBalances\(bytes32 poolId, IERC20\[\] memory expectedTokens\)
+        internal
+        view/g' contracts/vault/PoolBalances.sol
