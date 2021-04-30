329dbac115cc09f4570ed88d510644d1352ef943
Shelly Grossman <shellygr151@gmail.com>
2021-04-30 22:13:07 +0300

    update harness for oracle

--- verification/oracle/applyOracleHarness.sh
+++ verification/oracle/applyOracleHarness.sh
@@ -1,4 +1,7 @@
 # Replace LogExpMath with FakeLogExpMath
 perl -0777 -i -pe 's/import \"\.\/LogExpMath\.sol\";/import \"\..\/..\/..\/verification\/oracle\/FakeLogExpMath\.sol\";/g' contracts/lib/math/FixedPoint.sol
 perl -0777 -i -pe 's/ LogExpMath/ FakeLogExpMath/g' contracts/lib/math/FixedPoint.sol
+perl -0777 -i -pe 's/ LogExpMath/ FakeLogExpMath/g' contracts/pools/weighted/WeightedOracleMath.sol
+perl -0777 -i -pe 's/import \"\.\.\/\.\.\/lib\/math\/\/LogExpMath\.sol\";/import \"\..\/..\/..\/verification\/oracle\/FakeLogExpMath\.sol\";/g' contracts/pools/weighted/WeightedOracleMath.sol
+perl -0777 -i -pe 's/\(LogExpMath/\(FakeLogExpMath/g' contracts/pools/weighted/WeightedOracleMath.sol
 perl -0777 -i -pe 's/exp\(-x\)/3/g' contracts/lib/math/LogExpMath.sol
