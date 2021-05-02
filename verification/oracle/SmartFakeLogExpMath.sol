568e747b9cc61900c0a82b2a3287964e8bc71ef5
urikirsh <uri@certora.com>
2021-05-02 19:25:01 +0300

    smart fake math works

---
+++ verification/oracle/SmartFakeLogExpMath.sol
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: MIT
+
+
+pragma solidity ^0.7.0;
+
+/* solhint-disable */
+
+/**
+ * This file is an oversimplified, WRONG implementation of LogExpMath used for verification.
+ * We do NOT care about the mathematical soundness, so we do not mind what the return values are.
+ */
+library SmartFakeLogExpMath {
+    // All fixed point multiplications and divisions are inlined. This means we need to divide by ONE when multiplying
+    // two numbers, and multiply by ONE when dividing them.
+
+    // All arguments and return values are 18 decimal fixed point numbers.
+    int256 constant ONE_18 = 1e18;
+
+    // Internally, intermediate values are computed with higher precision as 20 decimal fixed point numbers, and in the
+    // case of ln36, 36 decimals.
+    int256 constant ONE_20 = 1e20;
+    int256 constant ONE_36 = 1e36;
+
+
+    // mapping(uint256 => mapping(uint256 => uint256)) constant map_pow;
+    /**
+     * @dev Exponentiation (x^y) with unsigned 18 decimal fixed point base and exponent.
+     *
+     * Reverts if ln(x) * y is smaller than `MIN_NATURAL_EXPONENT`, or larger than `MAX_NATURAL_EXPONENT`.
+     */
+    function pow(uint256 x, uint256 y) internal pure returns (uint256) {
+        if (y == 0) {
+            // We solve the 0^0 indetermination by making it equal one.
+            return uint256(ONE_18);
+        }
+
+        if (x == 0) {
+            return 0;
+        }
+
+        if (x == uint256(ONE_18)) {
+            return uint256(ONE_18);
+        }
+
+        // // The ln function takes a signed value, so we need to make sure x fits in the signed 256 bit range.
+        if (x >= 2 ** 255) return 2 ** 255 - 1; 
+
+        if (y == uint256(ONE_18)) {
+            return x;
+        }
+
+        if (y < uint256(ONE_18)) {
+            return x-1 > 0 ? x-1 : 1; 
+        } else {
+            // y > ONE_18
+            return x + 1 > 0 ? x : x+1; 
+        }
+        
+        // return map_pow[x][y];
+    }
+
+    // mapping(uint256 => uint256) constant map_exp;
+    /**
+     * @dev Natural exponentiation (e^x) with signed 18 decimal fixed point exponent.
+     *
+     * Reverts if `x` is smaller than MIN_NATURAL_EXPONENT, or larger than `MAX_NATURAL_EXPONENT`.
+     */
+    function exp(int256 x) internal pure returns (int256) {
+       if (x == 0) {
+           return ONE_18;
+       }
+    //    require (x+1 > 0);
+       if (x+1 == 0) return x;
+       return ONE_18 + 1 > x + 1 ? ONE_18 + 1 : x + 1;
+        // require(map_exp[x] > ONE_18);
+        // require(map_exp[x] > x);
+        // return map_exp[x];
+    }
+
+    // mapping(uint256 => mapping(uint256 => uint256)) constant log_map;
+    /**
+     * @dev Logarithm (log(arg, base), with signed 18 decimal fixed point base and argument.
+     */
+    function log(int256 arg, int256 base) internal pure returns (int256) {
+        // require (base > 0);
+        // require(arg > 0);
+        if (arg == ONE_18) return 0;
+        // uint256 res = log_map[arg][base];
+        // require (res < arg);
+        if (arg > ONE_18) {
+            return 1;
+            // require(res > 0);
+        } else {
+            // require(res < 0);
+            return -1;
+        }
+    }
+
+    // mapping(uint256 => uint256) constant ln_map;
+    /**
+     * @dev Natural logarithm (ln(a)) with signed 18 decimal fixed point argument.
+     */
+    function ln(int256 a) internal pure returns (int256) {
+        return _ln(a);
+    }
+
+    /**
+     * @dev Internal natural logarithm (ln(a)) with signed 18 decimal fixed point argument.
+     */
+    function _ln(int256 a) private pure returns (int256) {
+        // require(a > 0);
+        if (a == ONE_18) return 0;
+        // uint256 res = ln_map[a];
+        // require (res < a);
+        if (a > ONE_18) {
+            // require(res > 0);
+            return 1;
+        } else {
+            // require(res < 0);
+            return -1;
+        }
+        // return res;
+    }
+
+    /**
+     * @dev Intrnal high precision (36 decimal places) natural logarithm (ln(x)) with signed 18 decimal fixed point argument,
+     * for x close to one.
+     *
+     * Should only be used if x is between LN_36_LOWER_BOUND and LN_36_UPPER_BOUND.
+     */
+    function _ln_36(int256 x) private pure returns (int256) {
+        return _ln(x);
+    }
+}
--- verification/oracle/applyOracleHarness.sh
+++ verification/oracle/applyOracleHarness.sh
@@ -1,7 +1,7 @@
-# Replace LogExpMath with FakeLogExpMath
-perl -0777 -i -pe 's/import \"\.\/LogExpMath\.sol\";/import \"\..\/..\/..\/verification\/oracle\/FakeLogExpMath\.sol\";/g' contracts/lib/math/FixedPoint.sol
-perl -0777 -i -pe 's/ LogExpMath/ FakeLogExpMath/g' contracts/lib/math/FixedPoint.sol
-perl -0777 -i -pe 's/ LogExpMath/ FakeLogExpMath/g' contracts/pools/weighted/WeightedOracleMath.sol
-perl -0777 -i -pe 's/import \"\.\.\/\.\.\/lib\/math\/\/LogExpMath\.sol\";/import \"\..\/..\/..\/verification\/oracle\/FakeLogExpMath\.sol\";/g' contracts/pools/weighted/WeightedOracleMath.sol
-perl -0777 -i -pe 's/\(LogExpMath/\(FakeLogExpMath/g' contracts/pools/weighted/WeightedOracleMath.sol
+# Replace LogExpMath with SmartFakeLogExpMath
+perl -0777 -i -pe 's/import \"\.\/LogExpMath\.sol\";/import \"\..\/..\/..\/verification\/oracle\/SmartFakeLogExpMath\.sol\";/g' contracts/lib/math/FixedPoint.sol
+perl -0777 -i -pe 's/ LogExpMath/ SmartFakeLogExpMath/g' contracts/lib/math/FixedPoint.sol
+perl -0777 -i -pe 's/ LogExpMath/ SmartFakeLogExpMath/g' contracts/pools/weighted/WeightedOracleMath.sol
+perl -0777 -i -pe 's/import \"\.\.\/\.\.\/lib\/math\/\/LogExpMath\.sol\";/import \"\..\/..\/..\/verification\/oracle\/SmartFakeLogExpMath\.sol\";/g' contracts/pools/weighted/WeightedOracleMath.sol
+perl -0777 -i -pe 's/\(LogExpMath/\(SmartFakeLogExpMath/g' contracts/pools/weighted/WeightedOracleMath.sol
 perl -0777 -i -pe 's/exp\(-x\)/3/g' contracts/lib/math/LogExpMath.sol
