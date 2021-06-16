a244706cc226f635e642922e73f8444dc61c6bea
urikirsh <uri@certora.com>
2021-06-16 12:51:11 +0300

    debuggimg a vacuous rule, commiting before a pull

--- verification/AaveAssetManagerHarness.sol
+++ verification/AaveAssetManagerHarness.sol
@@ -17,17 +17,11 @@ contract AaveAssetManagerHarness is AaveATokenAssetManager {
         IERC20 _stkAave
     ) AaveATokenAssetManager(_vault, _token, _lendingPool, _aToken, _aaveIncentives, _stkAave) {}
 
-    // Simplifying fee calculation
-    mapping(uint256 => uint256) public amount_to_rebalance_fees;
-
     function _getRebalanceFee(
         uint256 poolCash,
         uint256 poolManaged,
         PoolConfig memory config
     ) internal view override returns (uint256) {
-        if (poolCash == 0) return 0;
-        uint256 _fee = amount_to_rebalance_fees[poolCash];
-        require(_fee < poolCash);
-        return _fee;
+        return 0;
     }
 }
--- verification/runCapitalMovement.sh
+++ verification/runCapitalMovement.sh
@@ -3,5 +3,6 @@ certoraRun verification/AaveAssetManagerHarness.sol verification/DummyERC20.sol 
   --solc solc7.6 \
   --optimistic_loop \
   --smt_timeout 300 \
+  --rule_sanity \
   --staging \
-  --msg "rule capital_out_decreases_investments with linked token"
+  --msg "rule capital_out_decreases_investments with sanity, no rebalance fee"
