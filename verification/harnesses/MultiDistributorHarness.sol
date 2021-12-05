4e3af5015d055e7f6d2456e95426775835e8a6d0
Aleksander Kryukov <firealexkryukov@gmail.com>
2021-12-05 13:54:16 +0200

    MultiDistrHarness

---
+++ verification/harnesses/MultiDistributorHarness.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.7.0;
+
+import "../munged/distributors/contracts/MultiDistributor.sol";
+
+contract MultiDistributorHarness is MultiDistributor {
+    constructor(IVault vault) MultiDistributor(vault) {
+        // solhint-disable-previous-line no-empty-blocks
+        // MultiDistributor is a singleton, so it simply uses its own address to disambiguate action identifiers
+    }
+
+    function getGlobalTokensPerStake(bytes32 distributionIds) public view returns (uint256){
+        return _distributions[distributionIds].globalTokensPerStake;
+    }
+
+    function getUserTokensPerStake(bytes32 distributionIds, IERC20 stakingToken, address sender) public view returns (uint256){
+        return _userStakings[stakingToken][sender].distributions[distributionId].userTokensPerStake;
+    }
+
+}
