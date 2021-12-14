f388aaa98367b2d1fe846b3649180ddd94576a4f
Aleksander Kryukov <firealexkryukov@gmail.com>
2021-12-15 00:15:59 +0200

    SymbolicVaultFixes and TotalAssetUser function

--- verification/harnesses/SymbolicVault.sol
+++ verification/harnesses/SymbolicVault.sol
@@ -10,13 +10,13 @@ contract SymbolicVault {
 
 
     //Store the balanceof from each token to account 
-    mapping (address  => mapping (address => uint)) public balanceOf;
+    mapping (IERC20  => mapping (address => uint)) public balanceOf;
 
     enum UserBalanceOpKind { DEPOSIT_INTERNAL, WITHDRAW_INTERNAL, TRANSFER_INTERNAL, TRANSFER_EXTERNAL }
     
     struct UserBalanceOp {
         UserBalanceOpKind kind;
-        address asset;
+        IERC20 asset;
         uint256 amount;
         address sender;
         address payable recipient;
@@ -29,10 +29,10 @@ contract SymbolicVault {
 
             address recipient = ops[i].recipient;
             address sender = ops[i].sender;
-            address asset = ops[i].asset;
+            IERC20 asset = ops[i].asset;
 
     		if (ops[i].kind ==  UserBalanceOpKind.DEPOSIT_INTERNAL) {
-    			IERC20(asset).transferFrom(sender, address(this),amount);
+    			IERC20(asset).transferFrom(sender, address(this), amount);
     			balanceOf[asset][recipient] = balanceOf[asset][recipient].add(amount);
     		}
     		else if (ops[i].kind ==  UserBalanceOpKind.WITHDRAW_INTERNAL) {
@@ -44,12 +44,13 @@ contract SymbolicVault {
     			balanceOf[asset][recipient] = balanceOf[asset][recipient].add(amount);
     		}
     		else if (ops[i].kind ==  UserBalanceOpKind.TRANSFER_EXTERNAL) {
-    			IERC20(asset).transferFrom(sender,recipient,amount);		
+    			IERC20(asset).transferFrom(sender, address(this), amount);
+                IERC20(asset).transfer(recipient, amount);	
     		}
     	}
     }
 
-    // totalAssetOfUSer(IERC20 asset, addres user) = asset.balanceOf(user)  + symbolicVault.balanceOf(asset,user)     //nned a dummy ERC for it
-    // dispatcher
-
+    function totalAssetsOfUser(IERC20 asset, address user) external returns (uint256, uint256, uint256){
+        return (asset.balanceOf(user), balanceOf[asset][user], asset.balanceOf(user).add(balanceOf[asset][user]));
+    }
 }
