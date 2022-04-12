b3a959176ba9864e990830f4317deaf446cf5fe4
Tom French <tom@tomfren.ch>
2022-04-12 17:25:13 +0100

    chore: move BalancerMinter to a minters directory

--- pkg/liquidity-mining/contracts/minters/BalancerMinter.sol
+++ pkg/liquidity-mining/contracts/minters/BalancerMinter.sol
@@ -18,10 +18,10 @@ import "@balancer-labs/v2-solidity-utils/contracts/openzeppelin/ReentrancyGuard.
 import "@balancer-labs/v2-solidity-utils/contracts/openzeppelin/SafeMath.sol";
 import "@balancer-labs/v2-solidity-utils/contracts/openzeppelin/EIP712.sol";
 
-import "./interfaces/IBalancerMinter.sol";
-import "./interfaces/IBalancerTokenAdmin.sol";
-import "./interfaces/IGaugeController.sol";
-import "./interfaces/ILiquidityGauge.sol";
+import "../interfaces/IBalancerMinter.sol";
+import "../interfaces/IBalancerTokenAdmin.sol";
+import "../interfaces/IGaugeController.sol";
+import "../interfaces/ILiquidityGauge.sol";
 
 contract BalancerMinter is IBalancerMinter, ReentrancyGuard, EIP712 {
     using SafeMath for uint256;
